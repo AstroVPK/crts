ce79618ca97106553a10f1c43f5d8599c49f2785
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-06-19 18:03:07 -0400

    Made plots save

--- python/testScript.py
+++ python/testScript.py
@@ -55,7 +55,9 @@ def _readCRTS(filename, path):
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-b', '--band', type = str, default = 'r', help = r'SDSS band')
+	parser.add_argument('-n', '--name', type = str, default = 'PG1302-102', help = r'CRTS Object name')
+	parser.add_argument('-b', '--band', type = str, default = 'V', help = r'CRTS band')
+	parser.add_argument('-pwd', '--pwd', type = str, default = os.environ['CRTSDATADIR'], help = r'CRTS data directory')
 	parser.add_argument('-nsteps', '--nsteps', type = int, default = 1000, help = r'Number of steps per walker')
 	parser.add_argument('-nwalkers', '--nwalkers', type = int, default = 25*psutil.cpu_count(logical = True), help = r'Number of walkers')
 	parser.add_argument('-pMax', '--pMax', type = int, default = 1, help = r'Maximum C-AR order')
@@ -82,6 +84,12 @@ if __name__ == '__main__':
 	parser.add_argument('--viewer', dest = 'viewer', action = 'store_true', help = r'Visualize MCMC walkers')
 	parser.add_argument('--no-viewer', dest = 'viewer', action = 'store_false', help = r'Do not visualize MCMC walkers')
 	parser.set_defaults(viewer = True)
+	parser.add_argument('--show', dest = 'show', action = 'store_true', help = r'Show figures?')
+	parser.add_argument('--no-show', dest = 'show', action = 'store_false', help = r'Do not show figures')
+	parser.set_defaults(show = False)
+	parser.add_argument('--savefig', dest = 'savefig', action = 'store_true', help = r'Save figures?')
+	parser.add_argument('--no-savefig', dest = 'savefig', action = 'store_false', help = r'Do not save figures')
+	parser.set_defaults(savefig = True)
 	args = parser.parse_args()
 
 	if (args.qMax >= args.pMax):
@@ -95,18 +103,23 @@ if __name__ == '__main__':
 	if (args.qMin < 0):
 		raise ValueError('qMin must be greater than or equal to 0')
 
-	tIn, magIn, magerrIn, yIn, yerrIn, maskIn = _readCRTS('PG 1302-102.dat', '/home/vish/code/trunk/crts/data')
-	PG1302_102 = libcarma.externalLC(name = 'PG 1302-102', band = 'V', t = tIn, y = yIn, yerr = yerrIn, mask = maskIn)
+	tIn, magIn, magerrIn, yIn, yerrIn, maskIn = _readCRTS(args.name + '.dat', args.pwd)
+	PG1302_102 = libcarma.externalLC(name = args.name, band = args.band, t = tIn, y = yIn, yerr = yerrIn, mask = maskIn)
 	PG1302_102.yunit = r'$F$ (Jy)'
-	lagsE, sfE, sferrE = PG1302_102.sf(newdt = PG1302_102.dt)
-
-	#PG1302_102.plot()
-	#PG1302_102.plotsf(newdt = PG1302_102.dt)
-	#plt.show()
-
 	PG1302_102.minTimescale = args.minTimescale
 	PG1302_102.maxTimescale = args.maxTimescale
 	PG1302_102.maxSigma = args.maxSigma
+
+	if args.savefig or args.show:
+		print 'Plotting light curve for %s'%(PG1302_102.name)
+		figName = os.path.join(args.pwd,'%s_LC.jpg'%(PG1302_102.name))
+		if not os.path.isfile(figName):
+			PG1302_102.plot()
+			if args.savefig:
+				plt.savefig(figName, dpi = 1000)
+			if args.show:
+				plt.show()
+			plt.clf()
 
 	taskDict = dict()
 	DICDict= dict()
@@ -137,24 +150,73 @@ if __name__ == '__main__':
 
 	loc0 = np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[0][0]
 	loc1 = np.where(bestTask.LnPosterior == np.max(bestTask.LnPosterior))[1][0]
-	
-	lbls = list()
-	for i in xrange(pBest):
-		lbls.append(r'$\tau_{AR, %d}$ ($d$)'%(i + 1))
-	for i in xrange(qBest):
-		lbls.append(r'$\tau_{MA, %d} ($d$)$'%(i))
-	lbls.append(r'Amp. ($F$ $d^{%2.1f}$)'%(qBest + 0.5 - pBest))
-	mcmcviz.vizTriangle(pBest, qBest, bestTask.timescaleChain, labelList = lbls, figTitle = r'PG1302-102')
-	
-	Theta = bestTask.Chain[:, loc0, loc1]
-	nt = libcarma.basicTask(pBest, qBest)
-	nt.set(PG1302_102.dt, Theta)
-	PG1302_102.dtSmooth = PG1302_102.dt
-	nt.smooth(PG1302_102)
-	PG1302_102.plot()
-	bestTask.plotsf(PG1302_102, newdt = PG1302_102.dt)
-	bestTask.plotacf(PG1302_102, newdt = PG1302_102.dt)
 
-	plt.show()
+	if args.savefig or args.show:
+		lblsTau = list()
+		for i in xrange(pBest):
+			lblsTau.append(r'$\tau_{AR, %d}$ ($d$)'%(i + 1))
+		for i in xrange(qBest):
+			lblsTau.append(r'$\tau_{MA, %d}$ ($d$)'%(i))
+		lblsTau.append(r'Amp. ($Jy$ $d^{%2.1f}$)'%(qBest + 0.5 - pBest))
+		try:
+			mcmcviz.vizTriangle(pBest, qBest, bestTask.timescaleChain, labelList = lblsTau, figTitle = r'PG1302-102 Timescales Chain')
+		except ValueError as err:
+			print str(err)
+		else:
+			print 'Plotting triangle plot of timescales for the %s-band light curve of %s'%(PG1302_102.band, PG1302_102.name)
+			figName = os.path.join(args.pwd,'%s_Tau.jpg'%(PG1302_102.name))
+			if not os.path.isfile(figName):
+				if args.savefig:
+					plt.savefig(figName, dpi = 1000)
+				if args.show:
+					plt.show()
+				plt.clf()
 
-	pdb.set_trace()
+		lblsTheta = list()
+		for i in xrange(pBest):
+			lblsTheta.append(r'$\alpha_{%d}$'%(i + 1))
+		for i in xrange(qBest + 1):
+			lblsTheta.append(r'$\beta_{%d}$'%(i))
+		try:
+			mcmcviz.vizTriangle(pBest, qBest, bestTask.Chain, labelList = lblsTheta, figTitle = r'PG1302-102 Coefficients Chain')
+		except ValueError as err:
+			print str(err)
+		else:
+			print 'Plotting triangle plot of parameters for the %s-band light curve of %s'%(PG1302_102.band, PG1302_102.name)
+			figName = os.path.join(args.pwd,'%s_Theta.jpg'%(PG1302_102.name))
+			if not os.path.isfile(figName):
+				if args.savefig:
+					plt.savefig(figName, dpi = 1000)
+				if args.show:
+					plt.show()
+				plt.clf()
+
+	if args.savefig or args.show:
+		print 'Plotting the %s-band light curve of %s'%(PG1302_102.band, PG1302_102.name)
+		figName = os.path.join(args.pwd,'%s_smoothLC.jpg'%(PG1302_102.name))
+		if not os.path.isfile(figName):
+			Theta = bestTask.Chain[:, loc0, loc1]
+			nt = libcarma.basicTask(pBest, qBest)
+			nt.set(PG1302_102.dt, Theta)
+			PG1302_102.dtSmooth = PG1302_102.dt
+			nt.smooth(PG1302_102)
+			PG1302_102.plot()
+			if args.savefig:
+				plt.savefig(figName, dpi = 1000)
+			if args.show:
+				plt.show()
+			plt.clf()
+
+
+		print 'Plotting the structure function of the %s-band light curve of %s'%(PG1302_102.band, PG1302_102.name)
+		figName = os.path.join(args.pwd,'%s_SF.jpg'%(PG1302_102.name))
+		if not os.path.isfile(figName):
+			bestTask.plotsf(PG1302_102, newdt = PG1302_102.dt)
+			if args.savefig:
+				plt.savefig(figName, dpi = 1000)
+			if args.show:
+				plt.show()
+			plt.clf()
+
+	if args.stop:
+		pdb.set_trace()
